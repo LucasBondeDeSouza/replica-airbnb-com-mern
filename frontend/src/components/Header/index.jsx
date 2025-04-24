4591c3b25e14fd3991f9223e21920de2c450acd5
LucasBondeDeSouza <bondelucas77@gmail.com>
2025-04-24 12:15:54 -0300

    atualização

--- frontend/src/components/Header/index.jsx
+++ frontend/src/components/Header/index.jsx
@@ -1,6 +1,5 @@
 import React from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faSearch, faUserCircle } from "@fortawesome/free-solid-svg-icons"
+import { Bars3Icon, MagnifyingGlassIcon, UserCircleIcon } from '@heroicons/react/24/solid'
 
 export default () => {
 
@@ -13,19 +12,20 @@ export default () => {
                     <p className="text-2xl font-bold text-primary-400">ashbnb</p>
                 </div>
 
-                <div className="flex items-center border border-gray-300 pr-4 pl-6 py-2 rounded-full shadow">
+                <div className="flex items-center border border-gray-300 pr-4 pl-6 py-2 rounded-full shadow-md">
                     <p className="pr-4 border-r border-r-gray-300">Qualquer lugar</p>
                     <p className="px-4 border-r border-r-gray-300">Qualquer semana</p>
                     <p className="px-4">Hóspedes?</p>
                     
-                    <div className="bg-primary-400 text-white rounded-full size-7 flex justify-center items-center cursor-pointer">
-                        <FontAwesomeIcon icon={faSearch} className="size-3" />
+                    <div className="bg-primary-400 rounded-full p-2 text-white cursor-pointer">
+                        <MagnifyingGlassIcon className="size-4" />
                     </div>
                 </div>
 
-                <div className="flex items-center border border-gray-300 pr-4 pl-6 py-2 rounded-full shadow">
-                    <FontAwesomeIcon icon={faUserCircle} className="size-8" />
-                    <FontAwesomeIcon icon={faBars} className="size-4" />
+                <div className="flex items-center border border-gray-300 pr-4 pl-6 py-2 rounded-full gap-2 shadow-md">
+                    <Bars3Icon className="size-5 text-gray-600" />
+                    <UserCircleIcon className="size-8 text-gray-600" />
+                    <p>Lucas</p>
                 </div>
             </div>
         </div>
