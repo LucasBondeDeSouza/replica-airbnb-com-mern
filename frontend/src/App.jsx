54bb1f3ee5ec58bba95e46867cabde7e2e38b57a
LucasBondeDeSouza <bondelucas77@gmail.com>
2025-05-28 14:27:25 -0300

    atualização

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ import NotFound from './pages/NotFound';
 axios.defaults.baseURL = 
   import.meta.env.MODE === 'development'
     ? "http://localhost:3000/api"
-    : "http://localhost:3000/api"
+    : "https://hashbnb.onrender.com/api"
 
 axios.defaults.withCredentials = true
 
