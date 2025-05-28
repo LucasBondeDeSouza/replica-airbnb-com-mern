75832a89b9acace26f2e7ce57c2b6dad7076020c
LucasBondeDeSouza <bondelucas77@gmail.com>
2025-05-28 15:22:33 -0300

    atualização

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,7 @@ export const __dirname = dirname(__filename)
 app.use(express.json())
 app.use(cookieParser())
 
-app.use(cors({
-    origin: 'http://localhost:5173',
-    credentials: true,
-}))
+app.use(cors())
 
 app.use("/tmp", express.static(__dirname + '/tmp'))
 app.use(express.static(path.join(__dirname, '../frontend/dist')))
